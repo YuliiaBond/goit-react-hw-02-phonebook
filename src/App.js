c0e1b7136dc307fe31490dae3569b1ea65cf7d43
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-07-16 23:57:23 +0300

    added style form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'; 
-
+import Form from './components/Form'
 class App extends Component {
+  state = {
+  contacts: [],
+    name: '',
+    number: ''
+  }
+  
+  formSubmitHandler = data => {
+    console.log(data);
+  };
 
   render() {
     return (
-      <h1>Hellooooooooooooo!</h1>
+      <>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.formSubmitHandler} />
+      </>
     )
   }
 }
